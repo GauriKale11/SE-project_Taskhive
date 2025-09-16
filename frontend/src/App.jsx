3511b2cd25e232face1e3ad81a36da3e6349464f
GauriKale11 <gaurikale110905@gmail.com>
2025-09-16 12:51:40 +0530

    Basic Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Dashboard from '../pages/Dashboard'
+import Login from '../pages/Login'
+import Task from '../pages/Task'
+import Profile from '../pages/Profile'
+import Navbar from '../components/Navbar'
+
+
+function App() {
+  
+  const router = createBrowserRouter([
+    {path: "/", element : <div><Navbar/><Dashboard/></div>},
+    {path: "/login", element : <div><Navbar/><Login/></div>},
+    {path: "/task", element : <div><Navbar/><Task/></div>},
+    {path: "/profile", element : <div><Navbar/><Profile/></div>},
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
