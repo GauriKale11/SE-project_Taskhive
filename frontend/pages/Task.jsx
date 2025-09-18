540a1b0b54a2f9262798a9abe97b0385a3fa1258
shweta-585 <sweta.app19@gmail.com>
2025-09-19 04:26:16 +0530

    Add add-quiz UI

--- frontend/pages/Task.jsx
+++ frontend/pages/Task.jsx
@@ -2,8 +2,41 @@ import React from 'react'
 
 const Task = () => {
   return (
-    <div>
+    <div className="add-task-container">
+        <div className="add-task-box">
+
+            <div className="add-form">
+                <label>Title</label>
+                <input type="text"/>
+            </div>
+
+            <div className="add-form"><div>
       <p>Task Page</p>
+    </div>
+                <label>Description</label>
+                <input type="text"/>
+            </div>
+
+            <div className="add-form">
+                <label>Due Date</label>
+                <input type="date"/>
+            </div>
+
+            <div className="add-form">
+                <label>Do you want to get notifications?</label>
+                <input type="checkbox"/>
+            </div>
+
+            <div className="add-form">
+                <label>Due Date</label>
+                <input type="date"/>
+            </div>
+
+            <div className="add-btn-div">
+                <button className="add-sub-btn">SUBMIT</button>
+            </div>
+
+        </div>
     </div>
   )
 }
