4afa94a20d24d49bde807747c9922030d63f363f
noopur <noopurkarkare@gmail.com>
2025-09-21 16:38:21 +0530

    basic profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import profileImage from "../assets/react.svg";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import "../styles/profile.css";
+
 
 const Profile = () => {
   return (
-    <div>
-      <p>User Profile Page</p>
+    <div className="personal">
+      <img src={profileImage} alt="" />
+      <h1 className="name">Name of user</h1>
+      <p className="country">Serbia</p>
+      <p className="stack">Front-End Web Developer</p>
+      <div className="buttons">
+        <button className="primary">Message</button>
+        <button className="secondary">Following</button>
+      </div>
+      <div className="social">
+      <FaLinkedin />
+      <FaXTwitter />
+      <FaGithub />
+      <FaYoutube />
+      <FaInstagram />
+      </div>
+      <div className='skills'>
+        <h2>Skills</h2>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javscript</li>
+            <li>React.js</li>
+            <li>Wordpress</li>
+            <li>SEO</li>
+        </ul>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
