63ac1eb4a0f6d2f91cc20fd26ceed5e4b7fff72b
GauriKale11 <gaurikale110905@gmail.com>
2025-09-25 15:45:46 +0530

    login and signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,15 @@ import Login from './pages/Login'
 import Task from './pages/Task'
 import Profile from './pages/Profile'
 import Navbar from './components/Navbar'
+import Signup from './pages/Signup'
 
 
 function App() {
   
   const router = createBrowserRouter([
     {path: "/", element : <div><Navbar/><Dashboard/></div>},
-    {path: "/login", element : <div><Navbar/><Login/></div>},
+    {path: "/login", element : <div><Login/></div>},
+    {path: "/signup", element: <Signup/>},
     {path: "/task", element : <div><Navbar/><Task/></div>},
     {path: "/profile", element : <div><Navbar/><Profile/></div>},
   ]);
