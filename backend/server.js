8096c28205294c7e0d08e00143efe2ed8bdd6eb5
noopur <noopurkarkare@gmail.com>
2025-09-26 22:09:48 +0530

    jwt node

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
@@ -10,5 +11,7 @@ app.use(express.json());
 app.post('/login',(req,res)=>{
     const username = req.body.username
     const user = {name:username}
-    jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken:accessToken})
 });
