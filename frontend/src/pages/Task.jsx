fbbfa2b62a6b9ae772cd9d0bf9d2dc91e8e8cc32
shweta-585 <sweta.app19@gmail.com>
2025-09-27 17:09:18 +0530

    Update styling of Task.jsx page

--- frontend/src/pages/Task.jsx
+++ frontend/src/pages/Task.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import "../styles/add-task.css";
+import logo from "../assets/logo.jpg";
 
 const Task = () => {
   return (
     <div className="add-task-container">
+        <div className="add-task-logo">
+            <img src={logo}  />
+        </div>
         <div className="add-task-box">
 
             <div className="add-form">
@@ -17,20 +21,24 @@ const Task = () => {
             </div>
 
             <div className="add-form">
+                <label>Start Date</label>
+                <input type="date"/>
+            </div>
+
+            <div className="add-form">
                 <label>Due Date</label>
                 <input type="date"/>
+            </div>
+
+            <div className="add-form">
+                <label>Default time of day</label>
+                <input type="number" />
             </div>
 
             <div className="add-form">
                 <label>Do you want to get notifications?</label>
                 <input type="checkbox"/>
             </div>
-
-            <div className="add-form">
-                <label>Due Date</label>
-                <input type="date"/>
-            </div>
-
             <div className="add-btn-div">
                 <button className="add-sub-btn">SUBMIT</button>
             </div>
